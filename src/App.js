ce24b9a0e0c60918bfe57e22dc7171047f09fdb3
Sarthak <sarthaksri017@gmail.com>
2023-07-15 22:12:42 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import CoreTeam from './Components/CoreTeam';
-
+import EBTeam from './Components/EB';
+import Team from './Components/Team';
 function App() {
   return (
    <>
   <Navbar />
+  <Team />
+  <EBTeam/>
    <CoreTeam />
    </>
   );
---
+++ src/Components/Team.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles1 from './Styles/team1.module.css';
+import img1 from '../Images/DrSonamDullat.jpg';
+import img2 from '../Images/team-1.jpg';
+function Team() {
+  return (
+    <div className={styles1.container}>
+      <div class={styles1.card}>
+        <img src={img2} alt="President" />
+        Dr Bhullar
+      </div>
+      <p>President</p>
+      <div class={styles1.card}>
+        <img src={img1} alt="Vice President" />
+        Dr Sonam Dullat
+      </div>
+      <p>Vice President</p>
+    </div>
+  )
+}
+
+export default Team
